9831aa4fbda965d90c7fbc15e044de49dfa1f6d2
luyang <634725546@qq.com>
2015-12-15 23:54:56 +0800

    return json object

--- src/main/java/bookshelf/controller/BookShelfController.java
+++ src/main/java/bookshelf/controller/BookShelfController.java
@@ -4,9 +4,7 @@ import bookshelf.model.Book;
 import bookshelf.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -16,62 +14,47 @@ public class BookShelfController {
     private BookService bookService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView queryBooks() {
+    public @ResponseBody Iterable<Book> queryBooks() {
 
-        ModelMap model = new ModelMap();
-        model.put("books", bookService.findAll());
-        return new ModelAndView("books", model);
-
+       return bookService.findAll();
     }
 
     @RequestMapping(value = "book/{isbn}", method = RequestMethod.GET)
-    public ModelAndView getBook(@PathVariable String isbn) {
+    public @ResponseBody Book getBook(@PathVariable String isbn) {
 
-        ModelMap model = new ModelMap();
-        model.put("book", bookService.findByIsbn(isbn));
-        return new ModelAndView("book", model);
+        return bookService.findByIsbn(isbn);
     }
 
     @RequestMapping(value = "book/new", method = RequestMethod.GET)
-    public ModelAndView createBook(){
-        return new ModelAndView( "newBook" , new ModelMap( new Book()) );
+    public @ResponseBody Book createBook(){
 
+        return  new Book();
     }
 
     @RequestMapping(value = "book", method = RequestMethod.POST)
-    public ModelAndView addBook(@RequestParam("isbn") String isbn, @RequestParam("name") String name,
-                                @RequestParam("author") String author, @RequestParam("price") Double price){
-        ModelMap modelMap = new ModelMap();
-        modelMap.put("books", bookService.addBook( isbn, name, author, price) );
-        return new ModelAndView( "books" , modelMap );
+    public @ResponseBody Iterable<Book> addBook(@RequestParam("isbn") String isbn, @RequestParam("name") String name,
+                                                @RequestParam("author") String author, @RequestParam("price") Double price){
 
+        return bookService.addBook( isbn, name, author, price);
     }
 
     @RequestMapping(value = "book/edit/{isbn}", method = RequestMethod.GET)
-    public ModelAndView editBook(@PathVariable String isbn) {
-        ModelMap modelMap = new ModelMap();
-        modelMap.put("book", bookService.findByIsbn( isbn ));
-        return new ModelAndView( "editBook" , modelMap );
+    public @ResponseBody Book editBook(@PathVariable String isbn) {
 
+        return bookService.findByIsbn( isbn );
     }
 
     @RequestMapping(value = "book/edit", method = RequestMethod.POST)
-    public ModelAndView updateBook(@RequestParam("isbn") String isbn, @RequestParam("name") String name,
-                                   @RequestParam("author") String author, @RequestParam("price") Double price) {
-        ModelMap modelMap = new ModelMap();
-        modelMap.put("book", bookService.updateBook( isbn, name, author, price) );
-        return new ModelAndView( "book" , modelMap );
+    public @ResponseBody Book updateBook(@RequestParam("isbn") String isbn, @RequestParam("name") String name,
+                                         @RequestParam("author") String author, @RequestParam("price") Double price) {
 
+        return bookService.updateBook( isbn, name, author, price);
     }
 
     @RequestMapping(value = "book/delete/{isbn}", method = RequestMethod.GET)
-    public ModelAndView deleteBook(@PathVariable String isbn) {
-        ModelMap modelMap = new ModelMap();
-        modelMap.put("books", bookService.deleteBook( isbn ));
-        return new ModelAndView( "books" , modelMap );
+    public @ResponseBody Iterable<Book> deleteBook(@PathVariable String isbn) {
 
+        return bookService.deleteBook( isbn );
     }
-
-
-
+    
 }
